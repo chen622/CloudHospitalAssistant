dfd1011677bd198efcbc5c247f6041415d104507
lucky98622 <745478424@qq.com>
2019-05-24 18:03:26 +0800

    删除无用mapper

--- Backend/src/main/java/cn/neuedu/his/util/inter/BaseMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.neuedu.his.util.inter;
-
-import tk.mybatis.mapper.common.IdsMapper;
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-
-public interface BaseMapper<T> extends Mapper<T>, MySqlMapper<T>, IdsMapper<T> {
-}
