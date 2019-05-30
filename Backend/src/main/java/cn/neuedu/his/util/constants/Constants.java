213b3271ce33ff0569327a426d47ad2b32f4fcd1
cjq <774143235@qq.com>
2019-05-30 11:35:17 +0800

    缴费（生成缴费单）

--- Backend/src/main/java/cn/neuedu/his/util/constants/Constants.java
+++ Backend/src/main/java/cn/neuedu/his/util/constants/Constants.java
@@ -143,7 +143,7 @@ public class Constants {
     public static final Integer REGISTRATION_PAYMENT_TYPE = 0;
     //处方项目费用类型
     public static final Integer NON_DRUG_PAYMENT_TYPE = 1;
-    //检验费用类型
+    //处方费用类型
     public static final Integer DRUG_PAYMENT_TYPE = 2;
 
 
