31329176d3accffddba8636ef5cfff41f95fe7d9
tys <928783527@qq.com>
2019-05-30 10:26:08 +0800

    删除部门

--- Backend/src/main/java/cn/neuedu/his/controller/DepartmentController.java
+++ Backend/src/main/java/cn/neuedu/his/controller/DepartmentController.java
@@ -9,10 +9,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -25,6 +22,12 @@ public class DepartmentController {
     @Autowired
     DepartmentService departmentService;
 
+    /**
+     * 获得一个部门的详细信息
+     * @param id
+     * @param authentication
+     * @return
+     */
     @GetMapping("/get/{id}")
     public JSONObject getDepartmentInformation(@PathVariable("id") Integer id, Authentication authentication){
 
@@ -42,4 +45,27 @@ public class DepartmentController {
 
         return CommonUtil.successJson(department);
     }
+
+    @PostMapping("/delete/{id}")
+    public JSONObject deleteDepartmentInformation(@PathVariable("id") Integer id, Authentication authentication){
+
+        //检查权限
+        try {
+            PermissionCheck.isHosptialAdim(authentication);
+        }catch (Exception e){
+            return CommonUtil.errorJson(ErrorEnum.E_602);
+        }
+
+        Department department = departmentService.getAllDepartmentInformation(id);
+
+        //检测部门是否存在
+        if (department == null)
+            return CommonUtil.errorJson(ErrorEnum.E_610);
+
+        departmentService.deleteById(id);
+
+        return CommonUtil.successJson(department);
+    }
+
+
 }
