bda2411d1d111f7eb7398bac24693bdd369cfcbc
lucky98622 <745478424@qq.com>
2019-05-31 18:20:29 +0800

    前端更新

--- EmployeeFronted/src/App.vue
+++ EmployeeFronted/src/App.vue
@@ -1,20 +1,67 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <a-layout>
+            <a-layout-header>
+                <a-menu
+                        v-model="current"
+                        mode="horizontal"
+                        :multiple="false"
+                >
+                    <a-menu-item key="index">
+                        <a-icon type="home"/>
+                        首页
+                    </a-menu-item>
+                    <a-menu-item key="app" disabled>
+                        <a-icon type="appstore"/>
+                        Navigation Two
+                    </a-menu-item>
+                    <a-sub-menu>
+                        <span slot="title" class="submenu-title-wrapper"><a-icon type="setting"/>Navigation Three - Submenu</span>
+                        <a-menu-item-group title="Item 1">
+                            <a-menu-item key="setting:1">Option 1</a-menu-item>
+                            <a-menu-item key="setting:2">Option 2</a-menu-item>
+                        </a-menu-item-group>
+                        <a-menu-item-group title="Item 2">
+                            <a-menu-item key="setting:3">Option 3</a-menu-item>
+                            <a-menu-item key="setting:4">Option 4</a-menu-item>
+                        </a-menu-item-group>
+                    </a-sub-menu>
+                    <a-menu-item key="alipay">
+                        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four -
+                            Link</a>
+                    </a-menu-item>
+                </a-menu>
+            </a-layout-header>
+            <a-layout-content>Content</a-layout-content>
+            <a-layout-footer>Footer</a-layout-footer>
+        </a-layout>
     </div>
-    <router-view/>
-  </div>
 </template>
 
+<script>
+    export default {
+        data: () => ({
+            current: ['mail'],
+        }),
+
+    }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    #app .ant-layout-header,
+    #app .ant-layout-footer {
+        background: #7dbcea;
+        color: #fff;
+    }
+
+    #app .ant-layout-footer {
+        line-height: 1.5;
+    }
 </style>
