25f7fb0873fc6c7351443a57e74253c44b6e00a1
lucky98622 <745478424@qq.com>
2019-06-02 23:36:59 +0800

    首页信息

--- EmployeeFronted/src/App.vue
+++ EmployeeFronted/src/App.vue
@@ -1,67 +1,110 @@
 <template>
     <div id="app">
-        <a-layout>
-            <a-layout-header>
-                <a-menu
-                        v-model="current"
-                        mode="horizontal"
-                        :multiple="false"
-                >
-                    <a-menu-item key="index">
-                        <a-icon type="home"/>
-                        首页
-                    </a-menu-item>
-                    <a-menu-item key="app" disabled>
-                        <a-icon type="appstore"/>
-                        Navigation Two
-                    </a-menu-item>
-                    <a-sub-menu>
-                        <span slot="title" class="submenu-title-wrapper"><a-icon type="setting"/>Navigation Three - Submenu</span>
-                        <a-menu-item-group title="Item 1">
-                            <a-menu-item key="setting:1">Option 1</a-menu-item>
-                            <a-menu-item key="setting:2">Option 2</a-menu-item>
-                        </a-menu-item-group>
-                        <a-menu-item-group title="Item 2">
-                            <a-menu-item key="setting:3">Option 3</a-menu-item>
-                            <a-menu-item key="setting:4">Option 4</a-menu-item>
-                        </a-menu-item-group>
-                    </a-sub-menu>
-                    <a-menu-item key="alipay">
-                        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four -
-                            Link</a>
-                    </a-menu-item>
-                </a-menu>
+        <a-layout style="background: #fff">
+            <a-layout-header class="header">
+                <a-row type="flex" align="middle" justify="start">
+                    <a-col :span="8">
+                        <img class="logo" src="./assets/logo/logo-grey-none.png">
+                    </a-col>
+                    <a-col :span="16">
+                        <a-menu class="right-title" v-model="current" mode="horizontal" :multiple="false">
+                            <a-menu-item key="index" @click="toRouter('/home')">
+                                <a-icon type="home"/>
+                                首页
+                            </a-menu-item>
+                            <a-menu-item key="user">
+                                <a-icon type="wechat"/>
+                                患者平台
+                            </a-menu-item>
+                            <a-sub-menu>
+                                <span slot="title" class="submenu-title-wrapper"><a-icon type="setting"/>控制台</span>
+                                <a-menu-item key="login">登录</a-menu-item>
+                                <a-menu-item key="about">关于</a-menu-item>
+                            </a-sub-menu>
+                        </a-menu>
+                    </a-col>
+                </a-row>
             </a-layout-header>
-            <a-layout-content>Content</a-layout-content>
-            <a-layout-footer>Footer</a-layout-footer>
+            <a-layout-content style="background: #fff">
+                <router-view></router-view>
+            </a-layout-content>
+            <a-layout-footer style="background: #000;color: white">
+                <a-row type="flex" justify="space-around" align="top">
+                    <a-col span="8">
+                        <img class="logo" src="./assets/logo/logo-white-none.png">
+                    </a-col>
+                    <a-col span="8">
+                        <p class="footer-title" >东软智慧医院</p>
+                        <p class="footer-info" >地址：沈阳市浑南区创新路66号</p>
+                        <p class="footer-info" >电话：024-66666666</p>
+                    </a-col>
+                </a-row>
+            </a-layout-footer>
         </a-layout>
     </div>
 </template>
 
 <script>
+    import ACol from "ant-design-vue/es/grid/Col";
+
     export default {
+        components: {ACol},
         data: () => ({
             current: ['mail'],
         }),
-
+        methods: {
+            toRouter: function (router) {
+                this.$router.push({path: router})
+            }
+        },
     }
 </script>
 
-<style>
-    #app {
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
+<style scoped>
+    .header {
+        background: #fff !important;
+        box-shadow: 0 5px 8px #8b8b8b;
+        position: relative;
+        z-index: 10;
+        max-width: 100%;
+        padding: 0 100px !important;
+        margin-bottom: 0;
+        height: 70px;
     }
 
-    #app .ant-layout-header,
-    #app .ant-layout-footer {
-        background: #7dbcea;
-        color: #fff;
+    .logo {
+        width: 214px;
+        height: 70px;
     }
 
-    #app .ant-layout-footer {
-        line-height: 1.5;
+    .right-title {
+        float: right;
+        height: 70px;
+    }
+
+
+    .ant-menu-item {
+        height: 70px;
+        line-height: 70px;
+        min-width: 72px;
+        font-size: 18px;
+
+    }
+
+    .ant-menu-submenu {
+        height: 70px;
+        line-height: 70px;
+        min-width: 72px;
+        font-size: 18px;
+
+    }
+
+    .footer-title {
+        font-size: 20px;font-weight: bold
+    }
+
+    .footer-info {
+        font-size: 14px;
+        margin-bottom: 4px;
     }
 </style>
