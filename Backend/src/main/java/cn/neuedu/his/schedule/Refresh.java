e00826eefb539343ff16d4914d1504a7b00175d1
lucky98622 <745478424@qq.com>
2019-06-06 10:31:03 +0800

    定时函数

---
+++ Backend/src/main/java/cn/neuedu/his/schedule/Refresh.java
@@ -0,0 +1,21 @@
+package cn.neuedu.his.schedule;
+
+import cn.neuedu.his.model.ConstantVariable;
+import cn.neuedu.his.service.ConstantVariableService;
+import cn.neuedu.his.util.constants.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Refresh {
+    @Autowired
+    ConstantVariableService constantVariableService;
+
+    @Scheduled(cron = "0 0/1 * * * ? ")
+    public void refreshConstant() {
+
+    }
+}
