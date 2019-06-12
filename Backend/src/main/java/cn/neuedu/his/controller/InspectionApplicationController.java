8777328dfd8af902ca399144f18b873f10c32617
tys <928783527@qq.com>
2019-06-12 23:40:25 +0800

    常量表操作更新

--- Backend/src/main/java/cn/neuedu/his/controller/InspectionApplicationController.java
+++ Backend/src/main/java/cn/neuedu/his/controller/InspectionApplicationController.java
@@ -197,20 +197,24 @@ public class InspectionApplicationController {
     }
 
 
-    @PostMapping("confirmApplication/{id}")
+    @PostMapping("/confirmApplication/{id}")
     public JSONObject confirmApplication(@PathVariable("id") Integer id, Authentication authentication){
 
-        Map<String, Object> data = (Map<String, Object>) authentication.getCredentials();
-
-        //判断权限
-        if (doctorService.findById((Integer) data.get("id"))== null){
-            return CommonUtil.errorJson(ErrorEnum.E_632);
+        try{
+            PermissionCheck.isHosptialAdim(authentication);
+        }catch (Exception e){
+            Map<String, Object> data = (Map<String, Object>) authentication.getCredentials();
+            //判断权限
+            if (doctorService.findById((Integer) data.get("id")) == null){
+                return CommonUtil.errorJson(ErrorEnum.E_632);
+            }
         }
+
         inspectionApplicationService.confirmApplication(id);
         return CommonUtil.successJson();
     }
 
-    @PostMapping("cancelApplication/{id}")
+    @PostMapping("/cancelApplication/{id}")
     public JSONObject cancelApplication(@PathVariable("id") Integer id,Authentication authentication){
 
         Map<String, Object> data = (Map<String, Object>) authentication.getCredentials();
@@ -223,7 +227,7 @@ public class InspectionApplicationController {
         return CommonUtil.successJson();
     }
 
-    @PostMapping("entryApplicationResult")
+    @PostMapping("/entryApplicationResult")
     public JSONObject entryApplicationResult(@RequestBody JSONObject jsonObject,Authentication authentication){
 
         Map<String, Object> data = (Map<String, Object>) authentication.getCredentials();
